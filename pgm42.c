f19fe69bcec5bd1f45f7217d44b681931bd4d42b
abdulazees15 <50805591+abdulazees15@users.noreply.github.com>
2019-05-29 15:41:39 +0530

    Update pgm42.c

--- pgm42.c
+++ pgm42.c
@@ -1,7 +1,23 @@
-a,b=input().split()
-if(a>l):
-    print(l)
-elif(a>l):
-    print(k)
-else:
-    print(l)
+#include<stdio.h>
+int main()
+{
+  	char s1[100], s2[100];
+  	int i;
+  	scanf("%s %s",&s1,&s2);
+  	for(i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++);
+		   
+  	if(s1[i] < s2[i])
+   	{
+   		printf("%s",s2);
+	}
+	else if(s1[i] > s2[i])
+   	{
+   		printf("%s",s1);
+	}
+	else
+   	{
+   		printf("%s",s1);
+	}
+  	
+  	return 0;
+}
